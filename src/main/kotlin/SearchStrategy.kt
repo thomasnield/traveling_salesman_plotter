4381edb9729d565e9dc87f850de79a8689ee2966
Thomas Nield <thomasnield@live.com>
2018-07-07 19:47:50 -0500

    wip

--- src/main/kotlin/SearchStrategy.kt
+++ src/main/kotlin/SearchStrategy.kt
@@ -98,8 +98,10 @@ enum class SearchStrategy {
             var bestSolution = Tour.toConfiguration()
 
             val tempSchedule = sequenceOf(
-                    generateSequence(80.0) { (it - .005).takeIf { it >= 0 } }
-            ).flatMap { it }
+                    generateSequence(80.0) { (it - .005).takeIf { it >= 30 } },
+                    generateSequence(0.0) { (it + .005).takeIf { it <= 80 } },
+                    generateSequence(80.0) { (it - .005).takeIf { it >= 30 } }
+                    ).flatMap { it }
             .toList().toTypedArray().toDoubleArray().let {
                 TempSchedule(200, it)
             }
